84a2205b1d77b429ad7803156e64cc05cccd4590
Chris Daley <chebizarro@gmail.com>
2018-04-14 23:36:55 +0000

    added typedefs to enums

--- include/mbgl-c/map/mode.h
+++ include/mbgl-c/map/mode.h
@@ -6,34 +6,33 @@
 extern "C"{
 #endif
 
-enum MbglMapMode {
+typedef enum MbglMapMode {
 	CONTINUOUS, // continually updating map
 	STATIC, // a once-off still image of an arbitrary viewport
 	TILE // a once-off still image of a single tile
-};
+} MbglMapMode;
 
-enum MbglConstrainMode {
+typedef enum MbglConstrainMode {
 	NONE,
 	HEIGHT_ONLY,
 	WIDTH_AND_HEIGHT
-};
+} MbglConstrainMode;
 
-enum MbglViewportMode {
+typedef enum MbglViewportMode {
 	DEFAULT,
 	FLIPPED_Y
-};
+} MbglViewportMode;
 
-enum MbglMapDebugOptions {
+typedef enum MbglMapDebugOptions {
 	NO_DEBUG     = 0,
 	TILE_BORDERS = 1 << 1,
 	PARSE_STATUS = 1 << 2,
 	TIMESTAMPS   = 1 << 3,
 	COLLISION    = 1 << 4,
 	OVERDRAW     = 1 << 5
-};
+} MbglMapDebugOptions;
 
 #ifdef __cplusplus
 }
 #endif
 #endif
-
